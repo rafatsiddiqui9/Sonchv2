2c7e759a2fca83a424d3d9f710e241dee47c8563
Rafat Siddiqui <mdrafat.siddiqui@outlook.com>
2024-02-13 20:25:30 +0530

    Initial Commit

---
+++ Sonchv2/Sonchv2App.swift
@@ -0,0 +1,17 @@
+//
+//  Sonchv2App.swift
+//  Sonchv2
+//
+//  Created by Rafat on 13/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Sonchv2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
